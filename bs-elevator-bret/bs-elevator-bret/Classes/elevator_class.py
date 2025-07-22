53b1c67179cebc354d112e4bdca7b25cbd3ab920
bmarshallpaycor <bmarshall@paycor.com>
2025-07-22 13:38:28 -0600

    Initial setup

---
+++ bs-elevator-bret/bs-elevator-bret/Classes/elevator_class.py
@@ -0,0 +1,4 @@
+class Elevator:
+    def __init__(self, number_Of_Floors):
+        self.number_Of_Floors = number_Of_Floors
+        self.Current_Floor = 0;
--- bs-elevator-bret/bs-elevator-bret/bs_elevator_bret.py
+++ bs-elevator-bret/bs-elevator-bret/bs_elevator_bret.py
@@ -1,1 +1,5 @@
-print("hello world")
+from Services import elevator_service
+
+
+elevator = elevator_service.setup_elevator_service()
+elevator_service.run_elevator(elevator)
